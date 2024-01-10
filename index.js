ea29b9df8cb98419e44a10a748a9a28ea589b43c
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-01-10 18:14:36 -0500

    Testing form submission

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const currentYear = new Date().getFullYear();
 document.querySelector(".year").innerHTML = currentYear;
 
 /* Form submission */
-// const form = document.querySelector("form");
-// const dialog = document.querySelector(".form-dialog");
-// const closeBtn = document.querySelector(".close-icon");
-// const nameInput = document.querySelector(".name-input");
-// const emailInput = document.querySelector(".email-input");
-// const messageInput = document.querySelector(".message-input");
+const form = document.querySelector("form");
+const dialog = document.querySelector(".form-dialog");
+const closeBtn = document.querySelector(".close-icon");
+const nameInput = document.querySelector(".name-input");
+const emailInput = document.querySelector(".email-input");
+const messageInput = document.querySelector(".message-input");
 
-// form.addEventListener("submit", (e) => {
-//   // e.preventDefault();
-//   dialog.showModal();
-// });
+form.addEventListener("submit", (e) => {
+  // e.preventDefault();
+  dialog.showModal();
+});
 
-// closeBtn.addEventListener("click", () => {
-//   dialog.close();
-//   nameInput.value = "";
-//   emailInput.value = "";
-//   messageInput.value = "";
-// });
+closeBtn.addEventListener("click", () => {
+  dialog.close();
+  nameInput.value = "";
+  emailInput.value = "";
+  messageInput.value = "";
+});
