d7cbafd8d92966a61880533752b2f847d4c27f4a
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-01-10 18:19:46 -0500

    Testing form submission

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ const nameInput = document.querySelector(".name-input");
 const emailInput = document.querySelector(".email-input");
 const messageInput = document.querySelector(".message-input");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  dialog.showModal();
-});
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      dialog.showModal();
+    })
+    .catch((error) => alert(error));
+};
+
+form.addEventListener("submit", handleSubmit);
 
 closeBtn.addEventListener("click", () => {
   dialog.close();
