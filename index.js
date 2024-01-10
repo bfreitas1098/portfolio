430b050687092ee7264dfd5ce3a65ba399d735af
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-01-10 17:52:49 -0500

    Built the dialog modal for when the contact form is submitted

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const year = document.querySelector(".year");
+/* Footer year */
 const currentYear = new Date().getFullYear();
-year.textContent = currentYear;
+document.querySelector(".year").innerHTML = currentYear;
+
+/* Form submission */
+const form = document.querySelector("form");
+const dialog = document.querySelector(".form-dialog");
+const closeBtn = document.querySelector(".close-icon");
+const nameInput = document.querySelector(".name-input");
+const emailInput = document.querySelector(".email-input");
+const messageInput = document.querySelector(".message-input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  dialog.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  dialog.close();
+  nameInput.value = "";
+  emailInput.value = "";
+  messageInput.value = "";
+});
