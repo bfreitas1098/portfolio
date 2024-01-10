4deafffba103e2330f0c4349fd7431f24f9b9266
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-01-10 18:46:23 -0500

    Created a custom success page when submitting the contact form

--- index.js
+++ index.js
@@ -10,26 +10,14 @@ const nameInput = document.querySelector(".name-input");
 const emailInput = document.querySelector(".email-input");
 const messageInput = document.querySelector(".message-input");
 
-const handleSubmit = (e) => {
-  e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   dialog.showModal();
+// });
 
-  const myForm = e.target;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => dialog.showModal())
-    .catch((error) => alert(error));
-};
-
-form.addEventListener("submit", handleSubmit);
-
-closeBtn.addEventListener("click", () => {
-  dialog.close();
-  nameInput.value = "";
-  emailInput.value = "";
-  messageInput.value = "";
-});
+// closeBtn.addEventListener("click", () => {
+//   dialog.close();
+//   nameInput.value = "";
+//   emailInput.value = "";
+//   messageInput.value = "";
+// });
