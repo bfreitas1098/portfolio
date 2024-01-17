7dab99b03bbf303f912b81b169e590e2db92f416
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-01-17 16:18:54 -0500

    Fixed form submission

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
 /* Footer year */
 const currentYear = new Date().getFullYear();
 document.querySelector(".year").innerHTML = currentYear;
-
-/* Form submission */
-const form = document.querySelector("form");
-const dialog = document.querySelector(".form-dialog");
-const closeBtn = document.querySelector(".close-icon");
-const nameInput = document.querySelector(".name-input");
-const emailInput = document.querySelector(".email-input");
-const messageInput = document.querySelector(".message-input");
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => alert("Thank you for your submission"))
-    .catch((error) => alert(error));
-};
-
-form.addEventListener("submit", handleSubmit);
