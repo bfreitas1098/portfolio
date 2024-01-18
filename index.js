e000cf0e53e9d02b9b3f5e8f542b258ca6ce6367
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-01-18 17:50:07 -0500

    Implemented smooth scrolling and optimized javascript loading

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const currentYear = new Date().getFullYear();
 document.querySelector(".year").innerHTML = currentYear;
 
 /* FormSubmit API setup */
+
 // fetch("https://formsubmit.co/api/get-apikey/bfreitas1098@hotmail.com", {
 //   method: "GET",
 // });
@@ -13,3 +14,25 @@ const apiKey =
 fetch(`https://formsubmit.co/api/get-submissions/${apiKey}`, {
   method: "GET",
 });
+
+/* Smooth scrolling */
+const aboutLink = document.querySelector(".nav-link--about");
+const testimonialsLink = document.querySelector(".nav-link--testimonials");
+const projectsLink = document.querySelector(".nav-link--projects");
+const contactLink = document.querySelector(".nav-link--contact");
+const aboutSection = document.getElementById("about-section");
+const testimonialsSection = document.getElementById("testimonials-section");
+const projectsSection = document.getElementById("projects-section");
+const contactSection = document.getElementById("contact-section");
+
+const scroll = function (section) {
+  section.scrollIntoView({ behavior: "smooth" });
+};
+
+aboutLink.addEventListener("click", scroll.bind(this, aboutSection));
+testimonialsLink.addEventListener(
+  "click",
+  scroll.bind(this, testimonialsSection)
+);
+projectsLink.addEventListener("click", scroll.bind(this, projectsSection));
+contactLink.addEventListener("click", scroll.bind(this, contactSection));
