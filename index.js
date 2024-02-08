edb590451a1a3047a776848dda80d84c9020890a
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-02-07 19:34:14 -0500

    Updated the index.css for all viewport widths up to 9999px

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ document.querySelector(".year").innerHTML = currentYear;
 
 /* FormSubmit API setup */
 
-fetch("https://formsubmit.co/api/get-apikey/bfreitas1098@hotmail.com", {
-  method: "GET",
-});
+// fetch("https://formsubmit.co/api/get-apikey/bfreitas1098@hotmail.com", {
+//   method: "GET",
+// });
 
-const apiKey =
-  "a7e537ec0e118c186c7a974c1be5e9093d7120d00b9cfb83230eb11144488757";
+// const apiKey =
+//   "a7e537ec0e118c186c7a974c1be5e9093d7120d00b9cfb83230eb11144488757";
 
-fetch(`https://formsubmit.co/api/get-submissions/${apiKey}`, {
-  method: "GET",
-});
+// fetch(`https://formsubmit.co/api/get-submissions/${apiKey}`, {
+//   method: "GET",
+// });
 
 /* Smooth scrolling */
 let aboutLink;
